83fa67f1249bebd905ed0288f093d552339ba9ed
kashkabash <a.v.kashkabash@tinkoff.ru>
2020-08-05 00:59:37 +0300

    added photo app

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,12 +1,24 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {PhotoService} from '../services/photo.service';
+import {Photo} from '../interfaces/photo.interface';
 
 @Component({
-  selector: 'app-tab1',
-  templateUrl: 'tab1.page.html',
-  styleUrls: ['tab1.page.scss']
+    selector: 'app-tab1',
+    templateUrl: 'tab1.page.html',
+    styleUrls: ['tab1.page.scss']
 })
-export class Tab1Page {
+export class Tab1Page implements OnInit {
+    photos: Photo[] = [];
 
-  constructor() {}
+    constructor(private photoService: PhotoService) {}
 
+    ngOnInit() {
+        this.photoService.loadSaved().then(() => {
+            this.photos = this.photoService.photos;
+        });
+    }
+
+    addPhotoToGallery() {
+        this.photoService.addNewToGallery();
+    }
 }
